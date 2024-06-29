7eae23611058aa784e529f268fef08b8fd5b6c58
Aleeze <162796538+Aleeze123@users.noreply.github.com>
2024-06-29 18:30:06 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.yellow.underline.bold("\n\t\t\t\t WELCOME TO ALEEZE COUNTDOWN TIMER..."));
+function startCountdown(duration: number) {
+    let remainingSeconds = duration;
+
+    const intervalId = setInterval(() => {
+        remainingSeconds--;
+
+        if (remainingSeconds <= 0) {
+            clearInterval(intervalId);
+            console.log(chalk.underline.bold.red("\t\t\t\tTime is up!"));
+        } else {
+            console.log(chalk.underline.bold.magentaBright(`Time remaining: ${remainingSeconds} seconds`));
+        }
+    }, 1000);
+}
+
+async function main() {
+    const { duration } = await inquirer.prompt([
+        {
+            type: "number",
+            name: "duration",
+            message: chalk.underline.bold.cyan("Enter the duration of the countdown timer (in seconds):"),
+            validate: (input: number) => {
+                return input > 0 ? true : chalk.underline.bold.red("Please enter a valid duration (greater than 0).");
+            }
+        }
+    ]);
+
+    startCountdown(duration);
+}
+
+main();
